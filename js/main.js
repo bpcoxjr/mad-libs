1f8e02493e6c1153aa33399a85378fb27b6e5bdd
Bruce P Cox Jr <bpcoxjr@yahoo.com>
2016-02-23 15:29:06 -0500

    added Angular controller

---
+++ js/main.js
@@ -0,0 +1,28 @@
+var madLibs = angular.module('ngMadLibs', [])
+
+madLibs.constant('VERSION', "1")
+madLibs.run(function(VERSION, $rootScope) {
+    $rootScope.version = VERSION;
+})
+
+madLibs.controller('madLibsController',function($scope){
+
+    $scope.data = {};
+
+    $scope.showMadlib = false;
+
+    $scope.submit = function(){
+        $scope.showMadlib = true;
+        $scope.submitted = true;
+        console.log("Submit clicked!");
+        console.log($scope.mad);
+    }
+
+    $scope.reset = function(){
+        $scope.mad = [];
+        $scope.showMadLib = false;
+        $scope.submitted = false;
+        console.log("Reset clicked!");
+        console.log($scope.mad);
+    }
+});
