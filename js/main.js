8937a546e3981bf16ea11fd78ba9fafdda25195a
JP DeVries <mail@devries.jp>
2016-02-24 10:51:42 -0800

    Fixing Reset function

--- js/main.js
+++ js/main.js
@@ -7,13 +7,35 @@ madLibs.run(function(VERSION, $rootScope) {
 
 madLibs.controller('madLibsController',function($scope){
 
-    $scope.data = {};
+    function initScope() {
+      $scope.gender = {
+      };
+
+      $scope.showMadLib = false;
+      $scope.submitted = false;
+
+    }
+
+    initScope();
 
     $scope.showMadlib = false;
 
+    $scope.setGender = function(gender) {
+      if(gender == 'male') {
+        $scope.gender.pronoun='he';
+        $scope.gender.possessive='his';
+        $scope.gender.poss='him';
+      } else {
+        $scope.gender.pronoun='she';
+        $scope.gender.possessive='her';
+        $scope.gender.poss='her';
+      }
+    }
+
     $scope.submit = function(){
+      console.log($scope);
         try {
-          if(typeof($scope.mad.gender) == 'undefined') {
+          if(!Object.keys($scope.gender).length) { // check if the object is empty
             genderError();
           }
         } catch (e) { genderError(); }
@@ -29,9 +51,10 @@ madLibs.controller('madLibsController',function($scope){
     }
 
     $scope.reset = function(){
-        $scope.mad = [];
-        $scope.showMadLib = false;
-        $scope.submitted = false;
+        document.getElementById('male').checked = document.getElementById('female').checked = false; // clear radio buttons visually
+        $scope.name = $scope.job = $scope.tedious = $scope.dirty_task = $scope.celebrity = $scope.useless = $scope.adjective = $scope.obnoxious_celebrity = $scope.huge_number = undefined;
+        initScope();
+
         console.log("Reset clicked!");
         console.log($scope.mad);
     }
